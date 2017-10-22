428ef4214e372e4fbf16914cfc1b3b40763131c7
KapilRijhwani <kapil.rijhwani@gmail.com>
2017-10-22 10:57:18 +0530

    Day 9: Recursion

---
+++ SolutionDay9.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+     private static int factorial(int n) {
+        if (n == 1) return 1;
+        return factorial(n - 1) * n;
+}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
+    }
+}
